33bcfafb289b30652b503c4c065f15d8e465f7b6
Lamashevski Dima <dima.lamashevski@gmail.com>
2018-10-31 14:56:02 +0300

    Add libs
    Make function GO

---
+++ src/GameTetris.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+
+public class GameTetris {
+
+	public static void main(String[] args) {
+		 new GameTetris().go();
+		 
+	}
+
+	private void go() {
+		 
+		
+	}
+
+}
